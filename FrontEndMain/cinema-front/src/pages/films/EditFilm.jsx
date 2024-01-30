bb18cb29f07f833608df71f3c982b1b26ff205aa
bordjos <bbordjoski97@gmail.com>
2024-01-30 18:50:17 +0100

    Added the base logic for the whole BackEnd section and the last update to the old FronEnd code

--- FrontEndMain/cinema-front/src/pages/films/EditFilm.jsx
+++
@@ -1,8 +0,0 @@
-import { useRouteLoaderData } from "react-router-dom";
-import FilmForm from "../../components/FilmForm";
-
-export default function EditFilm() {
-  const film = useRouteLoaderData("film-detail");
-
-  return <FilmForm film={film} method="put" />;
-}
--- FrontEndMain/cinema-front/src/util/logout.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from "react-router-dom";
-
-export function action() {
-  localStorage.removeItem("jwt");
-  localStorage.removeItem("role");
-  localStorage.removeItem("expiration");
-  return redirect("/");
-}
